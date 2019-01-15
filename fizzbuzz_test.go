8deb6e3564a360d7ff89292d7fe75faaa90a6ea5
warunya.chiangso@gmail.com <warunya.chiangso@gmail.com>
2019-01-15 15:24:05 +0700

    Update case 8 = 8

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -59,4 +59,12 @@ func TestFizzBuzz(t *testing.T) {
 			t.Errorf("If input is %d then result is %s, but result is %s", input, want, get)
 		}
 	})
+	t.Run("", func(t *testing.T) {
+		input := 8
+		want := "8"
+		get := FizzBuzz(input)
+		if get != want {
+			t.Errorf("If input is %d then result is %s, but result is %s", input, want, get)
+		}
+	})
 }
